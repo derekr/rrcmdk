41811316088f19a6e550df3d805b8ecffb9c1428
Derek Reynolds <derekr@users.noreply.github.com>
2024-04-05 05:40:42 -0700

    Add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -14,6 +15,12 @@ import {
   RouterProvider as TanstackRouterProvider,
   createMemoryHistory
 } from "@tanstack/react-router";
+
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 const rootRoute = createRootRoute({
   component: () => {
