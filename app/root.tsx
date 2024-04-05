2690e810730070745c38b200930c3cc4371d6919
Derek Reynolds <derekr@users.noreply.github.com>
2024-04-05 05:33:36 -0700

    Add tanstack router

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,65 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet as TanstackOutlet,
+  RouterProvider as TanstackRouterProvider,
+  createMemoryHistory
+} from "@tanstack/react-router";
+
+const rootRoute = createRootRoute({
+  component: () => {
+    return (
+      <div>
+        tanstack <TanstackOutlet />
+      </div>
+    );
+  },
+});
+
+const homeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/home",
+  component: function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>This is the home page.</p>
+      </div>
+    );
+  },
+});
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: function About() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>This is the about page.</p>
+      </div>
+    );
+  },
+});
+
+const routeTree = rootRoute.addChildren([homeRoute, aboutRoute]);
+
+const router = createRouter({
+  history: createMemoryHistory({ initialEntries: ['/'] }),
+  routeTree,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    // This infers the type of our router and registers it across your entire project
+    router: typeof rootRoute;
+  }
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -17,6 +76,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <TanstackRouterProvider router={router} />
         <ScrollRestoration />
         <Scripts />
       </body>
